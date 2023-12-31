84e516da3695fefdf49af3677c1cf62e3a139b74
Matt Czencz <Mattczencz24@gmail.com>
2023-12-30 22:10:26 -0700

    Extracted issues table and added loading skeletons

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,41 +1,15 @@
-import { Button, Table } from '@radix-ui/themes';
-import Link from 'next/link';
-import prisma from '@/prisma/client';
-import IssueStatusBadge from '../components/IssueStatusBadge';
+import { Suspense } from 'react';
+import IssueActions from './_components/IssueActions';
+import IssuesTable from './_components/IssuesTable';
+import IssuesTableSkeleton from './_components/IssuesTableSkeleton';
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
-
+const IssuesPage = () => {
   return (
     <div>
-      <div className="mb-6">
-        <Button><Link href="/issues/new">New Issue</Link></Button>
-      </div>
-      <Table.Root variant="surface">
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">Status</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">Created</Table.ColumnHeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {issues.map(issue => (
-            <Table.Row key={issue.id}>
-              <Table.Cell>
-                {issue.title}
-                <div className="block md:hidden">
-                  <IssueStatusBadge status={issue.status} />
-                </div>
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
-                <IssueStatusBadge status={issue.status} />
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">{issue.createdAt.toDateString()}</Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
+      <IssueActions />
+      <Suspense fallback={<IssuesTableSkeleton />}>
+        <IssuesTable />
+      </Suspense>
     </div>
   );
 };
