a1bdf46bde7d40dee08e47aef209d948128b5b7d
Matt Czencz <Mattczencz24@gmail.com>
2024-01-02 22:50:05 -0700

    Updated patch API to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,6 +1,6 @@
 import prisma from '@/prisma/client';
 import { NextRequest, NextResponse } from 'next/server';
-import issueSchema from '../issueSchema';
+import { patchIssueSchema } from '../issueSchema';
 import authOptions from '@/app/auth/authOptions';
 import { getServerSession } from 'next-auth';
 
@@ -14,10 +14,20 @@ export async function PATCH(
     return NextResponse.json({}, { status: 401 });
 
   const body = await request.json();
-  const validation = issueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
 
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
+
+  const { title, description, status, assignedToUserId } = body;
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: assignedToUserId }
+    });
+
+    if (!user)
+      return NextResponse.json({ error: 'Invalid user.' }, { status: 400 });
+  }
 
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) }
@@ -29,9 +39,10 @@ export async function PATCH(
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
     data: {
-      title: body.title,
-      description: body.description,
-      status: body.status
+      title,
+      description,
+      status,
+      assignedToUserId,
     }
   });
 
--- app/api/issues/issueSchema.ts
+++ app/api/issues/issueSchema.ts
@@ -1,11 +1,19 @@
 import { z } from 'zod';
 
 const issueSchema = z.object({
-  title: z.string().min(1, { message: 'Title is required.' }).max(255),
-  description: z.string().min(1),
-  status: z.enum(['OPEN', 'IN_PROGRESS', 'CLOSED']),
+  title: z.string().min(1, 'Title is required.').max(255),
+  description: z.string().min(1).max(65535),
 });
 
 export type TIssueForm = z.infer<typeof issueSchema>;
+
+export const patchIssueSchema = z.object({
+  title: z.string().min(1, 'Title is required.').max(255).optional(),
+  description: z.string().min(1, 'Description is required.').max(65535).optional(),
+  status: z.enum(['OPEN', 'IN_PROGRESS', 'CLOSED']).optional(),
+  assignedToUserId: z.string().min(1, 'AssignedToUserId is required.').max(255).optional().nullable()
+});
+
+export type TPatchIssueSchema = z.infer<typeof patchIssueSchema>;
 
 export default issueSchema;
