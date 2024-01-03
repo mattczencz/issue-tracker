7b647e955c813c4af449db6b91506f0142016e03
Matt Czencz <Mattczencz24@gmail.com>
2024-01-02 23:59:05 -0700

    Fixed bug where status was not updating

--- app/api/issues/issueSchema.ts
+++ app/api/issues/issueSchema.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 const issueSchema = z.object({
   title: z.string().min(1, 'Title is required.').max(255),
   description: z.string().min(1).max(65535),
+  status: z.enum(['OPEN', 'IN_PROGRESS', 'CLOSED']),
 });
 
 export type TIssueForm = z.infer<typeof issueSchema>;
