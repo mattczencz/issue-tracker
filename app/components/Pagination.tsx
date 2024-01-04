d0a85538874e79dcb1cc0841dde20f6194419693
Matt Czencz <Mattczencz24@gmail.com>
2024-01-03 17:29:09 -0700

    Implement pagination

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,5 +1,7 @@
+'use client';
 import { ChevronLeftIcon, ChevronRightIcon, DoubleArrowLeftIcon, DoubleArrowRightIcon } from '@radix-ui/react-icons';
 import { Button, Flex, Text } from '@radix-ui/themes';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 interface Props {
   itemCount: number;
@@ -8,23 +10,31 @@ interface Props {
 }
 
 const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
   const pageCount = Math.ceil(itemCount / pageSize);
+
+  const changePage = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', page.toString());
+    router.push('?' + params.toString());
+  };
 
   if (pageCount <= 1) return null;
 
   return (
     <Flex align="center" gap="4">
-      <Button variant="ghost" disabled={currentPage === 1}>
+      <Button variant="ghost" disabled={currentPage === 1} onClick={() => changePage(1)}>
         <DoubleArrowLeftIcon />
       </Button>
-      <Button variant="ghost" disabled={currentPage === 1}>
+      <Button variant="ghost" disabled={currentPage === 1} onClick={() => changePage(currentPage - 1)}>
         <ChevronLeftIcon />
       </Button>
       <Text size="2">Page {currentPage} of {pageCount}</Text>
-      <Button variant="ghost" disabled={currentPage === pageCount}>
+      <Button variant="ghost" disabled={currentPage === pageCount} onClick={() => changePage(currentPage + 1)}>
         <ChevronRightIcon />
       </Button>
-      <Button variant="ghost" disabled={currentPage === pageCount}>
+      <Button variant="ghost" disabled={currentPage === pageCount} onClick={() => changePage(pageCount)}>
         <DoubleArrowRightIcon />
       </Button>
     </Flex>
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import { Pagination } from './components';
 
-export default function Home() {
+export default function Home({ searchParams }: { searchParams: { page: string; }; }) {
   return (
     <main>
       <h1>Hello World!</h1>
       <Pagination
         itemCount={100}
         pageSize={10}
-        currentPage={10}
+        currentPage={parseInt(searchParams.page)}
       />
     </main>
   );
