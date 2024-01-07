e16d66b283757e3882b70ef21d62a2bf4cc9889e
Matt Czencz <Mattczencz24@gmail.com>
2024-01-06 21:54:17 -0700

    Fixed build error

--- app/issues/_components/SortingButtons.tsx
+++ app/issues/_components/SortingButtons.tsx
@@ -24,20 +24,35 @@ const SortingButtons = ({ searchParams }: Props) => {
   ];
 
   return (
-    <Flex gap="4" mb="4" align={{ initial: "start", md: "center" }} direction={{ initial: "column", md: "row" }}>
+    <Flex
+      gap="4"
+      mb="4"
+      align={{ initial: "start", md: "center" }}
+      direction={{ initial: "column", md: "row" }}
+    >
       <Text>Order by:</Text>
       <Flex gap="3" wrap="wrap">
         {options.map(option => (
-          <Button asChild size="1" variant="outline" className="hover:cursor-pointer" color={option.color}>
-            <Link
-              key={option.value}
-              href={{ query: { ...searchParams, orderBy: option.value } }}
-            >
+          <Button
+            asChild
+            key={option.value}
+            size="1"
+            variant="outline"
+            color={option.color}
+            className="hover:cursor-pointer"
+          >
+            <Link href={{ query: { ...searchParams, orderBy: option.value } }}>
               {option.label}
             </Link>
           </Button>
         ))}
-        <Button asChild size="1" variant="outline" className="hover:cursor-pointer" color="gray">
+        <Button
+          asChild
+          size="1"
+          variant="outline"
+          className="hover:cursor-pointer"
+          color="gray"
+        >
           <Link href={{ query: { ...searchParams, orderBy: null } }}>
             Clear Sorting
           </Link>
